79ae06b04c1f75995ece7c89b2ab7c589c7d8f7e
shivam chikara <s.chikara6885@gmail.com>
2024-08-30 06:04:06 +0000

    initial code

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div class='main-container'>
+    <h1>Congratulations</h1>
+    <div class='mini-container'>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' />
+      <h3>Kiran V</h3>
+      <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
